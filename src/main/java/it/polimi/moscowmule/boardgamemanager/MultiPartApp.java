eac5c5c0a034ac99137843362a31cff5988fd6c9
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-02-16 10:43:12 +0100

    Add support for MediaType.MULTIPART_FORM_DATA

---
+++ src/main/java/it/polimi/moscowmule/boardgamemanager/MultiPartApp.java
@@ -0,0 +1,15 @@
+package it.polimi.moscowmule.boardgamemanager;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/")
+public class MultiPartApp extends ResourceConfig{
+
+	public MultiPartApp(){
+		super(MultiPartFeature.class);
+	}
+	
+}
